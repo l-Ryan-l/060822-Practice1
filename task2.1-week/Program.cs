6202b89641cfe9d1b83d38d7e9edf84ba96ef455
Ryan <llryanll@yandex.ru>
2022-08-06 12:08:38 +0300

    add task2.1

---
+++ task2.1-week/Program.cs
@@ -0,0 +1,33 @@
+﻿//Программа которая будет выдавать день недели по номеру
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресение");
+        break;
+        
+    default:
+        Console.WriteLine("Такого дня недели нет. Уходите");
+        break;
+}
