5d4f42092cc2ee3c40f8907b5605d852d0e3ab84
Ryan <llryanll@yandex.ru>
2022-08-06 11:42:04 +0300

    add task2

---
+++ task2-switchCase/Program.cs
@@ -0,0 +1,17 @@
+﻿//Switch Case по сути условие которое проверятся последовательно и в случае выполнения не идет дальше.
+
+string name = "Валера";
+
+switch (name)
+{
+    case "Валера":
+        Console.WriteLine("Первое приветствие...");
+        break;
+    case "Петя":
+        Console.WriteLine("Второе приветствие...");
+        break;
+    default:
+        Console.WriteLine("Обычное приветствие...");
+        break;
+}
+
